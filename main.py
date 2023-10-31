2062feb7ad3a7a80ebd8f48b3500bb17c93d9349
Sofia Mañana <sofia.manana@sansano.usm.cl>
2023-10-30 23:43:45 -0300

    enviroment variables added to main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # Main for Gateway Side AlLoRa
 # HW: Raspberry Pi + TTGO Adapter
 
-import  sys
+import  sys, os
 
 from AlLoRa.Nodes.Gateway import Gateway
 from AlLoRa.Connectors.Serial_connector import Serial_connector
 
+serial_path = os.eviron.get('SERIAL_PATH', '/dev/ttyAMA3')
+volume_path = os.eviron.get('ALLORA_FILES')
 
 if __name__ == "__main__":
-	connector = Serial_connector(serial_port = '/dev/ttyAMA3', baud= '9600', timeout=1)
+	connector = Serial_connector(serial_port = serial_path, baud= '9600', timeout=1)
 	allora_gateway = Gateway(connector, config_file= "LoRa.json", debug_hops= False, TIME_PER_ENDPOINT=10)
 
 	# Listen to the digital_endpoints and print and save the files as they come in
